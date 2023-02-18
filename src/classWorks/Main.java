7e263fa8cb50a3190295009b0f1ac94734852e3e
Boriz81 <blitzboriz@gmail.com>
2023-02-18 21:22:59 +0900

    21:23

--- src/classWorks/Main.java
+++ src/classWorks/Main.java
@@ -15,3 +15,4 @@ public class Main {
 
 // https://docs.google.com/forms/d/e/1FAIpQLSep0aEbYth-WApnyJaVkNOq_tbRZvZWng18oN_6Lw7QOP1RNQ/viewscore?viewscore=AE0zAgBWePnQ7XU76593iddv1S45OYLyNDaUpYXWACBUmQU54CDPNGFQ_yzR3sM93UEpA_M
 // https://docs.google.com/forms/d/e/1FAIpQLSfARLQG1RKXIl8Rp0rqJqd0cJwza1Zvg5yIQwI4OAfhXJOVHQ/viewscore?viewscore=AE0zAgAxO9yusR_nfR-k3makw3GOIBEnKztf9NgKEHGVbnLK1KtShY_RrKLAYXjc8As4-NY
+// https://docs.google.com/forms/d/e/1FAIpQLSfXJN_7ENILvW5gj6W50f8EOeaEtfIvIn_EtglhIFacB1xFOQ/viewscore?viewscore=AE0zAgDMxCKEvzOktD_1mUiUJOcd48PfEJBLGKj47Y_kcynT6qJdg4F3JeGcT3gx5M6usiY
